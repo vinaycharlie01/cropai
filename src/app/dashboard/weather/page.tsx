a0ca70afcec146263638cbbda06daa017b200fd5
Phani Chandan <phanichandan02@gmail.com>
2025-07-25 09:38:33 +0000

    the weather forecast not working
    use weatherapi to get the info and wi

--- src/app/dashboard/weather/page.tsx
+++ src/app/dashboard/weather/page.tsx
@@ -41,6 +41,7 @@ const weatherIcons: { [key: string]: React.ReactNode } = {
   'Heavy rain': <CloudRain className="w-16 h-16 text-blue-400" />,
   Thunderstorm: <CloudLightning className="w-16 h-16 text-yellow-500" />,
   'Thundery outbreaks in nearby': <CloudLightning className="w-16 h-16 text-yellow-500" />,
+  'Thundery outbreaks possible': <CloudLightning className="w-16 h-16 text-yellow-500" />,
   Snow: <Snowflake className="w-16 h-16 text-blue-200" />,
   Mist: <Cloud className="w-16 h-16 text-gray-300" />,
   Fog: <Cloud className="w-16 h-16 text-gray-300" />,
@@ -50,34 +51,6 @@ const smallWeatherIcons: { [key: string]: React.ReactNode } = Object.entries(wea
     acc[key] = React.cloneElement(icon as React.ReactElement, { className: 'w-8 h-8' });
     return acc;
 }, {} as { [key: string]: React.ReactNode });
-
-const WeatherCardSkeleton = () => (
-    <div className="space-y-6">
-        <Card className="bg-background">
-            <CardHeader><Skeleton className="h-6 w-3/4" /></CardHeader>
-            <CardContent className="space-y-4">
-                 <div className="flex items-center justify-center space-x-6 p-4">
-                    <Skeleton className="w-20 h-20 rounded-full" />
-                    <div className="space-y-2">
-                        <Skeleton className="h-12 w-28" />
-                        <Skeleton className="h-5 w-24" />
-                    </div>
-                </div>
-                 <div className="space-y-2">
-                    {[...Array(5)].map((_, i) => <Skeleton key={i} className="w-full h-12 rounded-lg" />)}
-                 </div>
-            </CardContent>
-        </Card>
-        <Card className="bg-background">
-            <CardHeader><Skeleton className="h-6 w-1/2" /></CardHeader>
-            <CardContent className="space-y-2">
-                <Skeleton className="w-full h-5" />
-                <Skeleton className="w-full h-5" />
-                 <Skeleton className="w-3/4 h-5" />
-            </CardContent>
-        </Card>
-    </div>
-);
 
 const WeatherPage = () => {
     const [weatherData, setWeatherData] = useState<WeatherOutput | null>(null);
